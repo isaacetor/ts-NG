7c9608392571ce87ce42791e3f44e26352f75e2a
OgbuEsther <ogbuuzoma413@gmail.com>
2026-03-29 21:14:35 +0100

    home page

--- app/NG/page.tsx
+++ app/NG/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import Hero from './components/hero'
+import TrackSpending from './components/track-spending'
+import BuildSavings from './components/build-savings'
+import MoneyStructured from './components/money-structured'
+import NotJustAnotherApp from './components/not-just-another-app'
+import FinancialStrength from './components/financial-strength'
+import CreditThatWorks from './components/credit-that-works'
+import CashbackRewards from './components/cashback-rewards'
+import WhyDifferent from './components/why-different'
+import BuiltForNigerians from './components/built-for-nigerians'
+import HowWeMakeMoney from './components/how-we-make-money'
+import MoneyIsSafe from './components/money-is-safe'
+import RealStories from './components/real-stories'
+import CallToAction from './components/call-to-action'
+import Header from '@/app/_shared/header'
+import Footer from '@/app/_shared/footer'
 
-const NG = () => {
-  return <div>NG</div>;
-};
-
-export default NG;
+export default function Home() {
+  return (
+    <main className="bg-white font-sans">
+      <Header />
+      <Hero />
+      <NotJustAnotherApp />
+      <MoneyStructured />
+      <BuildSavings />
+      <TrackSpending />
+      <CashbackRewards />
+      <FinancialStrength />
+      <CreditThatWorks />
+      <WhyDifferent />
+      <BuiltForNigerians />
+      <HowWeMakeMoney />
+      <MoneyIsSafe />
+      <RealStories />
+      <CallToAction />
+      <Footer />
+    </main>
+  )
+}
