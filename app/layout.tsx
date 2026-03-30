57d7eb5f435b91a415f1bba10e6901abd33d220b
isaacetor <isaacetor7@gmail.com>
2026-03-29 21:22:48 -0800

    kk

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@300,400,500,700,900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
