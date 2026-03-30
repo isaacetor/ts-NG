4ba3f7d7924f466b10b4074bcf175c6448a190ee
isaacetor <isaacetor7@gmail.com>
2026-03-29 23:47:54 -0800

    credit page

--- app/NG/credit/page.tsx
+++ app/NG/credit/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import CreditHero from './_components/hero';
+import NG_Header from '../_shared/NG_header';
+import NG_Footer from '../_shared/NG_Footer';
 
 const Credit = () => {
-  return <div>Credit</div>;
+  return (
+    <div>
+      <NG_Header />
+      <CreditHero />
+      <NG_Footer />
+    </div>
+  );
 };
 
 export default Credit;
