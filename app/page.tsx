be0fb988da659b3b26909b052bff14a564ce7151
isaacetor <isaacetor7@gmail.com>
2026-03-30 16:33:40 -0800

    fixing changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Uk_Home from '@/app/uk/page';
+import Footer from './_shared/footer';
+import Header from './_shared/header';
+import AccrossBorders from './uk/_components/accrossBorders';
+import Feature1 from './uk/_components/feature1';
+import Feature2 from './uk/_components/feature2';
+import FiancialLife from './uk/_components/fiancialLife';
+import Hero from './uk/_components/hero';
+import Security from './uk/_components/security';
+import Testimonials from './uk/_components/testimonial';
+import ModernWorkingClass from './uk/_components/workingClass';
+
 const Home = () => {
   return (
-    <div>
-      <Uk_Home />
-    </div>
+    <main>
+      <Header />
+      <Hero />
+      <Feature1 />
+      <Feature2 />
+      <FiancialLife />
+      <ModernWorkingClass />
+      <AccrossBorders />
+      <Security />
+      <Testimonials />
+      <Footer />
+    </main>
   );
 };
 
